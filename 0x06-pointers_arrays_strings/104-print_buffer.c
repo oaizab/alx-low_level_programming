8f0dfd54bda18d89adfb05de564d63ad728cbec2
Omar Aizab <omaraizab98@gmail.com>
2023-03-03 19:31:04 +0100

    d

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", b[i + j]);
+			else
+				printf("  ");
+			if (j % 2)
+				printf(" ");
+		}
+		for (k = 0; k < 10; k++)
+		{
+			if (i + k < size)
+			{
+				if (b[i + k] >= ' ' && b[i + k] <= '~')
+					printf("%c", b[i + k]);
+				else
+					printf(".");
+			}
+		}
+		printf("\n");
+	}
+}
